aca0a70e8dcb693523929d2de4300b4c874e4fd1
JoannaKopiczak <59026378+JoannaKopiczak@users.noreply.github.com>
2020-01-18 14:35:57 +0100

    Add files via upload

---
+++ Rower.h
@@ -0,0 +1,23 @@
+		
+#pragma once
+#include<iostream>
+#include<vector>
+using namespace std;
+#include "Pojazd.h"
+
+class Rower : public Pojazd
+{
+public:
+	
+	string kolor;
+	string marka;
+	
+	Rower(string, string);
+	Rower();
+	
+	void zwrocRower();
+	void dodajRower();
+
+	vector<Rower> Rowery;
+
+};
