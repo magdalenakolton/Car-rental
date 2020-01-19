806391b5aa5740ee06a67361968403e79092dde2
JoannaKopiczak <59026378+JoannaKopiczak@users.noreply.github.com>
2020-01-19 12:44:59 +0100

    Add files via upload

---
+++ Samochod.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include "Motocykl.h"
+
+using namespace std;
+
+
+class Samochod : public Motocykl
+{
+public:
+
+	int ilosc_miejsc_siedzacych;
+	double pojemnoscbagaznika;
+
+	Samochod(int, double);
+	Samochod();
+
+	void dodajSamochod();
+	void zwrocSamochod();
+
+	vector<Samochod> Samochody;
+};
